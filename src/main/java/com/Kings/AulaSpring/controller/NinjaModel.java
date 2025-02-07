7bab6d2655b35c9e9d46f89a91ea6b1e6bfe9794
JReis <jedor.reis@gmail.com>
2025-02-07 09:11:40 -0300

    teste de amend

--- src/main/java/com/Kings/AulaSpring/controller/NinjaModel.java
+++ src/main/java/com/Kings/AulaSpring/controller/NinjaModel.java
@@ -9,9 +9,9 @@ import jakarta.persistence.*;
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    String nome, email;
-    int idade;
+    private Long id;
+    private String nome, email;
+    private int idade;
 
     public NinjaModel(int idade) {
 
@@ -20,6 +20,38 @@ public class NinjaModel {
     public NinjaModel(String nome, String email, int idade) {
         this.nome = nome;
         this.email = email;
+        this.idade = idade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
         this.idade = idade;
     }
 }
